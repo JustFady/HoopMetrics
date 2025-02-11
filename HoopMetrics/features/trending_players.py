7bc798cf4483379dd9ff4c88fcb75aa09d388cdb
Fady Youssef <62950351+JustFady@users.noreply.github.com>
2025-02-11 00:57:57 -0800

    Delete HoopMetrics/features directory

--- HoopMetrics/features/trending_players.py
+++
@@ -1,8 +0,0 @@
-import json
-import boto3
-
-def getTrendingPlayers(bucket, key):
-    s3 = boto3.client('s3')
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    data = json.loads(obj['Body'].read())
-    return sorted(data['players'], key=lambda x: x['points'], reverse=True)[:5]
